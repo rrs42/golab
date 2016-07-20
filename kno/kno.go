0784bde419ea5fb150ad1f585924f77b98e44ba6
Russell Steffen <rsteffen@bayarea.net>
2016-07-20 23:18:56 +0000

    Try line-based parsing

--- kno/kno.go
+++ kno/kno.go
@@ -10,23 +10,68 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 
-	"github.com/emirpasic/gods/sets/hashset"
-	"github.com/emirpasic/gods/stacks/linkedliststack"
+	set "github.com/emirpasic/gods/sets/hashset"
+	stack "github.com/emirpasic/gods/stacks/linkedliststack"
 )
 
+type vessel struct {
+	name       string
+	vesselType string
+}
+
 func main() {
-	keySections := hashset.New()
+	keySections := set.New()
 
 	keySections.Add("GAME")
 	keySections.Add("VESSEL")
 	keySections.Add("ROSTER")
 
-	openSaveFile("persistent.sfs", keySections)
+	parseSaveFile2("persistent.sfs", keySections)
 }
 
-func openSaveFile(fname string, keywords *hashset.Set) {
-	s := linkedliststack.New()
+func parseSaveFile2(fname string, keywords *set.Set) {
+	s := stack.New()
+
+	f, err := os.Open(fname)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	isParameter := func(line string) bool {
+		match, err := regexp.MatchString("\\w+ = \\w*", line)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		return match
+	}
+
+	var prevLine string
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "{" && keywords.Contains(prevLine) {
+			s.Push(prevLine)
+		} else if isParameter(line) {
+			parts := strings.Split(line, " = ")
+			fmt.Println(parts)
+		} else {
+			prevLine = line
+		}
+	}
+
+	fmt.Println("\nLast Token")
+	fmt.Print(s)
+}
+
+func parseSaveFile(fname string, keywords *set.Set) {
+	s := stack.New()
 
 	f, err := os.Open(fname)
 	if err != nil {
@@ -44,7 +89,9 @@ func openSaveFile(fname string, keywords *hashset.Set) {
 			s.Push(prevToken)
 		} else if token == "}" {
 			frame, _ := s.Pop()
-			fmt.Println(frame)
+			if frame != nil {
+				fmt.Println(frame)
+			}
 		} else {
 			prevToken = token
 		}
